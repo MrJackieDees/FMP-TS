fdffa75e672b2390990616b51213fe8cfa0fcc47
MrJackieDees <mejackiedees@gmail.com>
2026-04-01 00:31:02 +0100

    Reset

---
+++ Assets/Scripts/ResetScript.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ResetScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
